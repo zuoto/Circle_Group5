5331bd271e1b0a86c563da237e24e1a9e02b098c
minci <emku96@gmail.com>
2025-10-27 14:07:38 +0100

    "minor syntax fix to make it visible in browser"

--- circle/src/features/Feed.jsx
+++ circle/src/features/Feed.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Post from "../reusable-components/Post.jsx";
-import {mockPosts} from '../mock-data/feed-mock-data/mock-posts-and-comments.jsx';
+import { mockPosts } from "../mock-data/feed-mock-data/MockDataPostAndComment.jsx";
 export default function Feed() {
   return (
     <div className="page-wrapper">
       <div className="feature-names">Feed</div>
       <div className="main-content">
-         {mockPosts.map((post) => (
+        {mockPosts.map((post) => (
           <Post key={post.id} post={post} />
         ))}
       </div>
