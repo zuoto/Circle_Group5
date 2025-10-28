fca6787c4e917ad27f9a7ed64ba095f66f280b92
minci <emku96@gmail.com>
2025-10-28 18:58:51 +0100

    "added some css"

--- circle/src/features/Profile.jsx
+++ circle/src/features/Profile.jsx
@@ -1,8 +1,36 @@
-export default function Profile() {
+import React, { useState, useEffect } from "react";
+import "../index.css";
+import { users } from "../mock-data/mock-data-user/MockDataUsers.jsx";
+
+function Profile() {
+  const [user, setUser] = useState(null);
+
+  function loadUser() {
+    const found = users.find((u) => u.id === "u5");
+    setUser(found);
+  }
+
+  useEffect(() => {
+    loadUser(1);
+  }, []);
+
   return (
-    <div>
-      <h1>Profile</h1>
-      <p>User profile page.</p>
-    </div>
+    user && (
+      <div className="profile-name">
+        <img
+          src={user.avatar}
+          alt={`${user.name} avatar`}
+          style={{ width: "150px", height: "150px", borderRadius: "50%" }}
+        />
+        <div className="name">
+          <h1>
+            {" "}
+            {user.name} {user.surname}{" "}
+          </h1>
+        </div>
+      </div>
+    )
   );
 }
+
+export default Profile;
