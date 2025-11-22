57aa5c344d6ebabcda2cd0d2521ced5a659dd753
minci <emku96@gmail.com>
2025-11-22 18:18:03 +0100

    added components, refactored, fixed some profile pic issues

---
+++ circle/src/components/ProfileGroupLink.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function ({ group }) {
+  return (
+    <Link
+      key={group.id}
+      to={`/groups/${group.id}`}
+      className="group-card-link-compact"
+    >
+      <div className="group-card-compact">
+        <h3 style={{ margin: 0 }}>{group.name}</h3>
+        <div className="member-count-box">
+          <span className="member-count-number">{group.memberCount}</span>
+          <span className="member-count-text">members</span>
+        </div>
+      </div>
+    </Link>
+  );
+}
